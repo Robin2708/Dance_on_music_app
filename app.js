45ccc6a1dc73f2ea5568d122698a11da2d0783d5
Robin2708 <122463489+Robin2708@users.noreply.github.com>
2025-05-01 15:37:55 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ const PORT = 8000;
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
 
 module.exports = app;
